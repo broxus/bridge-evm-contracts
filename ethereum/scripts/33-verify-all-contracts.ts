3f1136f7acf1935e3cee995593c97d4dbb1ae2f9
Alexander <fairyfromalfeya@gmail.com>
2025-01-21 22:03:49 +0100

    feat: added script to verify all evm contracts

--- ethereum/scripts/33-verify-all-contracts.ts
+++ ethereum/scripts/33-verify-all-contracts.ts
@@ -4,12 +4,14 @@ const main = async () => {
     const deploys = await deployments.all().then((a) => Object.entries(a));
 
     for (const dep of deploys) {
-        console.log(`Verify ${dep[0]}: ${dep[1].address}`);
+        if (dep[1].bytecode) {
+            console.log(`Verify ${dep[0]}: ${dep[1].address}`);
 
-        await run("verify:verify", {
-            address: dep[1].address,
-            constructorArguments: dep[1].args,
-        });
+            await run("verify:verify", {
+                address: dep[1].address,
+                constructorArguments: dep[1].args,
+            });
+        }
     }
 };
 
