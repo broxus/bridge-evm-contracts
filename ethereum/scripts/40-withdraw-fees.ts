081c7bd1445ed16fc2d47fab9638b7b3a1118207
Елена Яковлева <el.iakovv@gmail.com>
2025-02-28 20:55:06 +0100

    fix withdraw-fees script

--- ethereum/scripts/40-withdraw-fees.ts
+++ ethereum/scripts/40-withdraw-fees.ts
@@ -1,30 +1,58 @@
-import { deployments, ethers, getNamedAccounts } from "hardhat";
+import { ethers, getNamedAccounts } from "hardhat";
 
 const tokens: Record<string, string[]> = {
-    main: [],
-    bsc: [],
-    fantom: [],
-    polygon: [],
-    avalanche: [],
+  main: [],
+  bsc: [],
+  fantom: [],
+  polygon: [],
+  avalanche: [],
 };
 
 const main = async () => {
-    const { name: networkName } = await ethers.provider.getNetwork();
-    console.log(`Network name: ${networkName}`);
+  const { name: networkName, chainId } = await ethers.provider.getNetwork();
+  console.log(`Network name: ${networkName}`);
 
-    const networkTokens = tokens[networkName];
+  const networkTokens = tokens[networkName];
 
-    const { management } = await getNamedAccounts();
-    console.log(`Management: ${management}`);
+  const { management } = await getNamedAccounts();
+  console.log(`Management: ${management}`);
 
-    for (const token of networkTokens) {
-        console.log(`Skim ${token}`);
-        const tx = await deployments.execute("MultiVault", { from: management, log: true }, "skim", [token]);
-        console.log(`Tx hash: ${tx.transactionHash}`)
-    }
+  const multivault = await ethers.getContract("MultiVault");
+
+  const transactions = [];
+  for (const token of networkTokens) {
+    transactions.push({
+      to: await multivault.getAddress(),
+      value: "0",
+      data: null,
+      contractMethod: {
+        inputs: [{ internalType: "address", name: "token", type: "address" }],
+        name: "skim",
+        payable: false,
+      },
+      contractInputsValues: { token: token },
+    });
+  }
+
+  const json = {
+    version: "1.0",
+    chainId: chainId.toString(),
+    createdAt: new Date().getTime(),
+    meta: {
+      name: "Skim Batch",
+      description: "",
+      txBuilderVersion: "1.18.0",
+      createdFromSafeAddress: management.toString(),
+      createdFromOwnerAddress: "",
+      checksum: "0x2278322fc7e9159d3d46832c1866c94311186dd9920f6d31c15af0242f0f4e35",
+    },
+    transactions: transactions,
+  };
+
+  console.log(JSON.stringify(json));
 };
 
 main().catch(error => {
-    console.error(error);
-    process.exitCode = 1;
+  console.error(error);
+  process.exitCode = 1;
 });
