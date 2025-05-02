f8800841c6d656d317bed0e783942e92ec9aebb6
Елена Яковлева <el.iakovv@gmail.com>
2025-05-02 14:57:22 +0200

    feat: only evm contracts

--- everscale/test/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export const isValidTonAddress = (address: string) =>
-  /^(?:-1|0):[0-9a-fA-F]{64}$/.test(address);
--- everscale/test/utils/logger.ts
+++
@@ -1,7 +0,0 @@
-import { Address } from "locklift";
-
-export const logContract = async (name: string, address: Address) => {
-  const balance = await locklift.provider.getBalance(address);
-
-  console.log(`${name} (${address}) - ${locklift.utils.fromNano(balance)}`);
-};
